06004125c752a5e5219aeb3ef0a5105c085d9eac
lesornithorynquesasthmatiques <vazlin@gmail.com>
2013-01-16 14:03:40 +0100

    Essai Mongo

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
   routes = require('./routes'),
-  api = require('./routes/api');
+  api = require('./routes/api'),
+  mongo = require('./routes/mongo');
 
 var app = module.exports = express();
 
-// Configuration
-
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
@@ -28,19 +20,17 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
+//// Routes
 app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
 
 // JSON API
-
 app.get('/api/name', api.name);
+
+// test mongo
+app.get('/mongo/kittensWhoseNameStartsWithFluff', mongo.kittensWhoseNameStartsWithFluff);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
-
-// Start server
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
