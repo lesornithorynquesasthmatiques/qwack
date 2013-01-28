871242be84ade56d76b4393d2641669696f0cd72
lesornithorynquesasthmatiques <vazlin@gmail.com>
2013-01-28 13:51:42 +0100

    Update batch/install_hdf5_libs.sh
    missing $

--- batch/install_hdf5_libs.sh
+++ batch/install_hdf5_libs.sh
@@ -12,7 +12,7 @@ MAVEN_HDF5_GROUPID="org.hdfgroup"
 MAVEN_HDF5_VERSION="2.9"
 
 function mvnInstall(){
-    mvn install:install-file -Dfile="$HDF5_DIR/$1.jar" -DgroupId="MAVEN_HDF5_GROUPID" -DartifactId=$1 -Dversion=$MAVEN_HDF5_VERSION -Dpackaging=jar
+    mvn install:install-file -Dfile="$HDF5_DIR/$1.jar" -DgroupId="$MAVEN_HDF5_GROUPID" -DartifactId=$1 -Dversion=$MAVEN_HDF5_VERSION -Dpackaging=jar
 }
 
 mvnInstall jhdf
