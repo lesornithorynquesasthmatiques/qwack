31558f468192bbe03c6b7f7442242a7b77ae933a
lesornithorynquesasthmatiques <vazlin@gmail.com>
2013-02-06 15:01:20 +0100

    artist controller

--- webapp/app/js/artist.js
+++ webapp/app/js/artist.js
@@ -2,4 +2,13 @@
 
 function ArtistCtrl($scope, Artist) {
 
+    $scope.mbid = 'bfcc6d75-a6a5-4bc6-8282-47aec8531818';
+
+    $http.get('/lastfm/artist/'+ $scope.mbid).
+        success(function(data, status, headers, config) {
+            $scope.lastFmInfo = data;
+        }).
+        error(function(data, status, headers, config) {
+            console.log("ERRORRRRR", data, status, headers, config);
+        });
 }
