8cd53ff748bcef53879290b006154413432dd79f
ldoin <loicdoin@MacBook-Pro-de-Loic.local>
2013-02-06 15:23:35 +0100

    verification sur l'egalite des mots de passe i Please enter the commit message for your changes. Lines starting

--- webapp/app/js/register.js
+++ webapp/app/js/register.js
@@ -1,8 +1,32 @@
-function RegisterCtrl($scope) {
+function RegisterCtrl($scope, $location, User) {
 	
 	$scope.register = function() {
-		console.log("register user " + $scope.email)
+		if(arePasswordsDifferent()){
+			
+			console.log("password and password confirmation aren't identical")
+		} else {
+			user = new User({
+				email: $scope.email,
+				password: $scope.password,
+				passwordVerify: $scope.passwordConfirmation,
+				city: $scope.town
+			});
+			
+			user.$save(
+				function(){
+					console.log("user is registred : " + JSON.stringify(user));
+					$location.path('/news');
+				},
+				function(){
+					console.log("Registration failed for user :" + JSON.stringify(user));
+			});
+		}
 	};
 	
+	var arePasswordsDifferent = function() {
+		return $scope.password !== $scope.passwordConfirmation
+	};
+	
+	
 }
-RegisterCtrl.$inject = ['$scope'];
+RegisterCtrl.$inject = ['$scope', '$location', 'User'];
