5dfda471e0c388a0011259ec2806869d258b4b81
lesornithorynquesasthmatiques <vazlin@gmail.com>
2013-02-06 16:11:41 +0100

    Start myPage

--- webapp/app/js/mypage.js
+++ webapp/app/js/mypage.js
@@ -2,4 +2,16 @@
 
 function MyPageCtrl($scope, $http) {
 
+ 	$scope.searchArtist = function(){
+
+ 		if (this.searchKey.length>3){
+              $http.get('/api/artists/find?q='+ this.searchKey).
+			      success(function(data, status, headers, config) {
+			          $scope.searchResult = data;
+			        }).
+			      error(function(data, status, headers, config) {
+			        console.log("ERRORRRRR", data, status, headers, config);
+			      });
+        }
+ 	}
 }
