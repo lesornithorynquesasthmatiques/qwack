36312b8b724d5d419affc4a6796652c54356a099
lesornithorynquesasthmatiques <vazlin@gmail.com>
2013-02-06 12:02:04 +0100

    [Nicolas] Formulaire de login

--- webapp/app/js/artists.js
+++ webapp/app/js/artists.js
@@ -1,10 +1,6 @@
 'use strict';
 
-/* Controllers */
-
 function ArtistListCtrl($scope, Artist) {
   $scope.artists = Artist.query();
   $scope.orderProp = 'name';
 }
-
-//PhoneListCtrl.$inject = ['$scope', 'Phone'];
